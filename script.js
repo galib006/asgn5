0c153bfd54a2ba7e92c3bfb42ad3ec041299b119
galib006 <galibmaruf@gmail.com>
2025-08-28 22:08:32 +0600

    Add JS

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
+let HeartBtns = document.querySelectorAll('.heartBtn');
+let copys = document.querySelectorAll('.copy');
+let calls = document.querySelectorAll('.call');
 
-let HeartBtn = document.querySelectorAll('.heartBtn');
-let heartCount = document.getElementById('heartCount');
+
+let copyCount = document.getElementById('heartCount');
 let heartClick = 0;
-HeartBtn.addEventListener('click',function(){
-    HeartBtn.style.color="red";
-    heartClick++;
-    heartCount.innerText = heartClick
-    
-})
+let copyClick = 0;
+for (let HeartBtn of HeartBtns) {
+    HeartBtn.addEventListener('click', function() {
+        let HeartIcon = HeartBtn.querySelector('.heartBtn i');
+        HeartIcon.classList.remove('fa-regular');
+        HeartIcon.classList.add('fa-solid');
+        HeartBtn.style.color = "red";
+        heartClick++;
+        let heartCount = document.getElementById('heartCount');
+        heartCount.innerText = heartClick;
 
+    });
+}
+for (let copy of copys) {
+    copy.addEventListener('click', function() {
+        let card = copy.closest('.card');
+        let copyTitle = card.querySelector('.copyTitle');
+        let phnNo = card.querySelector('.phnNo');
+        let copyPhnno = phnNo.innerText;
+        navigator.clipboard.writeText(copyPhnno);
+        alert(copyTitle.innerHTML + 'NO. Copy Successfull')
+        copyClick++;
+        let copyCount = document.getElementById('copyCount');
+        copyCount.innerText = copyClick
+
+    });
+}
+
+for (let call of calls) {
+    call.addEventListener('click', function() {
+        let card = call.closest('.card');
+        let Subtitle = card.querySelector('.cardSubtitle');
+        let phnNO = card.querySelector('.phnNo');
+        alert(`Calling ${Subtitle.innerText} ${phnNO.innerText}...`)
+
+
+    })
+}
