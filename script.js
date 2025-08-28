d1bf6851bc1a7135e9f89a3f19edf68256cf5b89
galib006 <galibmaruf@gmail.com>
2025-08-28 17:15:26 +0600

    update some feature

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+let HeartBtn = document.querySelectorAll('.heartBtn');
+let heartCount = document.getElementById('heartCount');
+let heartClick = 0;
+HeartBtn.addEventListener('click',function(){
+    HeartBtn.style.color="red";
+    heartClick++;
+    heartCount.innerText = heartClick
+    
+})
+
